0a2045f298d64e5a87dbf471fb547996505aca46
WataruNishimura <110705525+wataru1199@users.noreply.github.com>
2022-10-18 13:44:40 +0000

    add_customers_index_Nishi

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,29 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+  root :to => "homes#top"
+  get '/about' => 'homes#about'
 
+  resources :items, only: [:index, :show]
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :customers, only: [:show, :edit, :update, :confirm, :withdraw]
+  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+  resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+  resources :destinations, only: [:index, :edit, :create, :update, :destroy]
 
   namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :homes, only: [:top]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: [:update]
+    end
   end
+
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
