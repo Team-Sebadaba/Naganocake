a910d14b5442b59cd4c5fa62d150e86809caec3f
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-10-19 17:11:40 +0000

    [uodate]enum_install

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,9 +1,10 @@
 class Detail < ApplicationRecord
   belongs_to :item
   belongs_to :order
-  
+
   enum item_status: { impossible_making: 0, waiting: 1, maiking: 2, complete: 3 }
-  
+
   # ItemとOrderのモデルが出来たら以下の記述を加える。
   # has_many :post_comments, dependent: :destroy
+
 end
