546cf20189a4145c7c09be547b84ed8550b15ebf
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-10-19 04:12:33 +0000

    [add]top.html.erb追記

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
-    @customers = Customer.all.page(params[:page])
+    @details = Detail.all.page(params[:page])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root :to => 'homes#top'
     resources :sessions, only: [:new, :create, :destroy]
-    resources :homes, only: [:top]
+    # resources :homes, only: [:top]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
