9158d1fe2977640741af8a3e030ff661ed960455
WataruNishimura <110705525+wataru1199@users.noreply.github.com>
2022-10-19 15:07:35 +0000

    add_genres_index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_18_192016) do
+ActiveRecord::Schema.define(version: 2022_10_19_021312) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2022_10_18_192016) do
     t.integer "item_status", default: 0, null: false
     t.integer "amount", null: false
     t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
