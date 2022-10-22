1a4f1a657aea0104c81f890d0b6b096463f5b564
sfjdkfjjfjfdk <112384285+sfjdkfjjfjfdk@users.noreply.github.com>
2022-10-22 06:27:28 +0000

    orders_controller_customer_hasegawa

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
   # namespace :public do
     # get 'destinations/index'
     # get 'destinations/edit'
