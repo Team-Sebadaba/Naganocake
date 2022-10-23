964e3bdd5a06f2935fff980bbdb61b5718e2d279
Rjeholge <111187701+Rjeholge@users.noreply.github.com>
2022-10-23 15:30:53 +0000

    [Add]注文機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,57 +1,52 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'orders/new'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  # namespace :public do
-    # get 'destinations/index'
-    # get 'destinations/edit'
-  # end
-
-
-   devise_for :customers,skip: [:passwords], controllers: {
-   registrations: "public/registrations",
-   sessions: 'public/sessions'
- }
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
+  # public(会員側)
+  # homes
   root :to => "public/homes#top"
-
-
   get '/about' => 'public/homes#about'
-  # get '/admin' => 'homes#top'
 
-   get '/customers' => 'public/customers#show'
-   get '/customers/infomation/edit' => 'public/customers#edit', as: 'edit_customer'
-   patch '/customers/infomation' => 'public/customers#update'
-   get '/customers/confirm' => 'public/customers#confirm'
-   patch 'customers/withdraw' => 'public/customers#withdraw'
+  # customers
+  get '/customers' => 'public/customers#show'
+  get '/customers/infomation/edit' => 'public/customers#edit', as: 'edit_customer'
+  patch '/customers/infomation' => 'public/customers#update'
+  get '/customers/confirm' => 'public/customers#confirm'
+  patch 'customers/:id/withdraw' => 'public/customers#withdraw', as: 'withdraw_customer'
 
-   get '/destinations' => 'public/destinations#index'
-   post '/destinations' => 'public/destinations#create'
-   patch 'destinations/:id' => 'public/destinations#update', as: 'update_destination'
-   get '/destinations/:id/edit' => 'public/destinations#edit', as: 'edit_destination'
-   delete '/destinations/:id' => 'public/destinations#destroy', as: 'destroy_destination'
+  # destinations
+  get '/destinations' => 'public/destinations#index'
+  post '/destinations' => 'public/destinations#create'
+  patch 'destinations/:id' => 'public/destinations#update', as: 'update_destination'
+  get '/destinations/:id/edit' => 'public/destinations#edit', as: 'edit_destination'
+  delete '/destinations/:id' => 'public/destinations#destroy', as: 'destroy_destination'
 
-#   resources :items, only: [:index, :show]
-   get '/items' => 'public/items#index'
-   get '/items/:id' => 'public/items#show'
+  # orders
+  get '/orders/new' => 'public/orders#new'
+  get '/orders' => 'public/orders#index'
+  get '/orders/:id' => 'public/orders#show'
+  post '/orders/create' => 'public/orders#create'
+  post '/orders/confirm' => 'public/orders#confirm'
+  get '/orders/complete' => 'public/orders#complete'
 
-  # resources :sessions, only: [:new, :create, :destroy]
-  # resources :customers, only: [:show, :edit, :update, :confirm, :withdraw]
+  # items
+  get '/items' => 'public/items#index'
+  get '/items/:id' => 'public/items#show'
+
+  # cart_items
   resources :cart_items, only: [:index, :update, :destroy, :create]
   delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
-  resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
-# resources :destinations, only: [:index, :edit, :create, :update, :destroy]
 
+  # admin(管理者側)
   namespace :admin do
     root :to => 'homes#top'
-    # resources :sessions, only: [:new, :create, :destroy]
     resources :homes, only: [:top]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
@@ -61,7 +56,4 @@ Rails.application.routes.draw do
     end
   end
 
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
