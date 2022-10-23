2823fde547f5f07d75a0a9afccb82c44d283c783
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-10-23 04:51:16 +0000

    [add]注文履歴詳細

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,7 @@
 class Admin::OrderDetailsController < ApplicationController
+    def update
+        @order_detail = Detail.find(params[:id])
+        @order_detail.update(order_params)
+        redirect_to admin_order_path
+    end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,13 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @customer = Customer.find(params[:id])
+    @ditail = Ditails.find(params[:id])
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path
   end
 end
