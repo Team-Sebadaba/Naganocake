00018f817f964983435305596ed7b6ad17fa90cb
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-10-23 19:44:34 +0000

    [add]public商品一覧レイアウト

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,7 +2,7 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.all.page(params[:page]).per(8)
-    #@genres = Genre.all
+    @genres = Genre.all
   end
 
   def show
