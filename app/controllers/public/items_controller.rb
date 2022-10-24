80fd2a4370f2a6a615cefdcbe37cf973d4fa1184
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-10-24 03:46:17 +0000

    商品詳細ページレイアウト途中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,7 +8,7 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem
-    #@genres = Genre.all
+    @genres = Genre.all
   end
 
   private
