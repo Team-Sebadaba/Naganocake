de1fd3b84842ad18c52340814ad784114ba39343
hayashiyukino <112701375+hayashiyukino@users.noreply.github.com>
2022-10-27 06:05:04 +0000

    [update]製作ステータス機能修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,26 +1,25 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    # 1件の注文詳取得
     @order_detail = Detail.find(params[:id])
+    # byebug
     @order = @order_detail.order
-    @order_details = @order_details
-    @item_status = Detail.where(item_status: "maiking")
+    @order_details = @order.details
     @order_detail.update(order_detail_params)
-    if @item_status.count >= 1
+    if @order_details.where(item_status: "maiking").count >= 1
       @order.order_status = "maiking"
       @order.save
     end
-    if @item_status.count == Detail.where(item_status: "complete").count
+    if @order.details.count == @order_details.where(item_status: "complete").count
       @order.order_status = "ready_to_ship"
       @order.save
-    redirect_to admin_order_path(@order_detail.order.id)
     end
+    redirect_to admin_order_path(@order_detail.order.id)
   end
 
   private
 
   def order_detail_params
-    params.require(:detail).permit(:order_id, :item_id, :item_status, :amount, :price)
+    params.require(:detail).permit(:item_status)
   end
 
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,20 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @details = @order.details.all
+    @details = @order.details
     @subtotal_price = 0
   end
 
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
-    @order_details = @order.order_details
-    if @order.order_status == "payment"
+    @order_details = @order.details
+    if @order.order_status == "waiting_payment"
+      @order_details.each do |order_detail|
+        order_detail.item_status  = "impossible_making"
+        order_detail.save
+      end
+    elsif @order.order_status == "payment"
       @order_details.each do |order_detail|
         order_detail.item_status  = "waiting"
         order_detail.save
@@ -21,7 +26,7 @@ class Admin::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:customer_id, :payment, :order_status, :postage, :total, :shipping_postal, :shipping_address, :shipping_name)
+    params.require(:order).permit(:order_status)
   end
 
 end
